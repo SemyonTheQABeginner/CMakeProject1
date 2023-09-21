209fa3006498a209ef125a6f5691809479e49150
Student <Student@PC-SE5113-21>
2023-09-21 11:55:55 +0200

    Add project files.

---
+++ CMakeProject1/CMakeProject1.cpp
@@ -0,0 +1,11 @@
+﻿// CMakeProject1.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeProject1.h"
+#include<stdio.h>
+
+int main()
+{
+	printf("Where Cyrillic letters");
+	return 0;
+}
